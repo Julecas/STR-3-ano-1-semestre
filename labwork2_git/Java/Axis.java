4bb2488c9e4cc8a8f7e5cbe5db0ead0f49d86a7b
Júlio Lopes <105436098+Julecas@users.noreply.github.com>
2023-11-23 19:59:57 +0000

    Delete labwork2_git/Java/Axis.java

--- labwork2_git/Java/Axis.java
+++
@@ -1,9 +0,0 @@
-public interface Axis {
-    public void moveForward();
-    public void moveBackward();
-    public void stop();
-    public int getPos();
-
-    //to implement in JAVA  
-    public void gotoPos(int pos);
-}
